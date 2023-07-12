8186d35e61de1e34c9fc7253b0b8d0e3e514c867
Denis Vasilev Dimchev <denisvasilev.dimchev@eng.it>
2023-07-12 11:14:08 +0200

    Compleated Lessons 1 and 2

---
+++ Lesson_3/build/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let myName = "Denis";
+let number;
+let isLoading;
+let album;
+myName = "Denis";
+number = 42;
+isLoading = true;
+album = 12;
+album = "Test";
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ Lesson_3/src/main.ts
@@ -0,0 +1,20 @@
+let myName: string = "Denis";
+let number: number;
+let isLoading: boolean;
+let album: string | number;
+
+myName = "Denis";
+number = 42;
+isLoading = true;
+
+album = 12;
+album = "Test";
+
+const sum = (a: number, b: string) => {
+    return a + b;
+}
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
