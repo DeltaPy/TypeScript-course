02ef3f11121451a81e719259d2c1c6eb92876cbe
DeltaPy <denis.dimchev@gmail.com>
2023-07-14 15:09:36 +0200

    Compleated Lesson 3

--- Lesson_3/build/js/main.js
+++ Lesson_3/build/js/main.js
@@ -1,16 +1,52 @@
 "use strict";
-let myName = "Denis";
-let number;
-let isLoading;
-let album;
-myName = "Denis";
-number = 42;
-isLoading = true;
-album = 12;
-album = "Test";
-const sum = (a, b) => {
-    return a + b;
+let stringArray = ["one", "hey", "denis"];
+let guitars = ["Strat", "Les Paul", 5150];
+let mixedData = ["EVH", 1984, true];
+stringArray[0] = "john";
+stringArray.push("hey");
+guitars[0] = 1984;
+guitars.unshift("Jim");
+// guitars = stringArray;
+// mixedData = guitars
+let test = [];
+let bands = [];
+bands.push("Van Halen");
+//Tuple
+let myTupple = ["Dave", 42, true];
+let mixed = ["John", 1, false];
+mixed = myTupple;
+//Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj); //object
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: 'Denis',
+    prop2: true,
 };
-let postId;
-let isActive;
-let re = /\w+/g;
+exampleObj.prop2 = false;
+let evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "OU812"]
+};
+let JP = {
+    name: "Jimmy",
+    albums: ['I', 'II', "III"]
+};
+const greatGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name}!`;
+    }
+};
+//Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- Lesson_3/src/main.ts
+++ Lesson_3/src/main.ts
@@ -1,20 +1,73 @@
-let myName: string = "Denis";
-let number: number;
-let isLoading: boolean;
-let album: string | number;
+let stringArray = ["one", "hey", "denis"];
 
-myName = "Denis";
-number = 42;
-isLoading = true;
+let guitars = ["Strat", "Les Paul", 5150];
 
-album = 12;
-album = "Test";
+let mixedData = ["EVH", 1984, true];
 
-const sum = (a: number, b: string) => {
-    return a + b;
+stringArray[0] = "john";
+stringArray.push("hey");
+
+guitars[0] = 1984;
+guitars.unshift("Jim");
+
+// guitars = stringArray;
+// mixedData = guitars
+
+let test = [];
+let bands: string[] = [];
+bands.push("Van Halen");
+
+//Tuple
+let myTupple: [string, number, boolean] = ["Dave", 42, true];
+
+let mixed = ["John", 1, false];
+mixed = myTupple;
+
+//Objects
+let myObj: object;
+myObj = [];
+console.log(typeof myObj); //object
+myObj = bands;
+myObj = {};
+
+const exampleObj ={
+    prop1: 'Denis',
+    prop2: true,
+};
+
+exampleObj.prop2 = false
+
+type Guitarist = {
+    name: string,
+    active?: boolean,
+    albums: (string | number)[]
 }
 
-let postId: string | number;
-let isActive: number | boolean;
+let evh: Guitarist = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "OU812"]
+}
+let JP: Guitarist = {
+    name: "Jimmy",
+    albums: ['I', 'II', "III"]
+}
 
-let re: RegExp = /\w+/g;
+const greatGuitarist = (guitarist: Guitarist) => {
+    if(guitarist.name) {
+        return `Hello ${guitarist.name}!`
+    }
+}
+
+//Enums
+
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade.U);
+
